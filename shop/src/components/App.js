737d17e0915ccc8116fc885b05541d5c34a20e9c
msinkeeva <msinkeeva@bk.ru>
2023-04-29 16:07:44 +0600

    медиа запросы

--- shop/src/components/App.js
+++ shop/src/components/App.js
@@ -1,12 +1,6 @@
 import Router from "./Router"
 import main from "./styles/main.scss"
-import navbar from "./styles/navbar.scss"
-import home from "./styles/homePage.scss"
-import registration from "./styles/registration.scss"
-import catalog from "./styles/catalog.scss"
-import search from "./styles/search.scss"
-import product from "./styles/product.scss"
-import cart from "./styles/cart.scss"
+
 
 function App(){
     return (
